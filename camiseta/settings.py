cd28c56810b2790dfcf5c132e128c8e2b05c8bec
kamilacamacho1 <mccamacho79@misena.edu.co>
2016-08-31 14:57:21 -0500

    'udenar'

--- camiseta/settings.py
+++ camiseta/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deporte1.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'static/'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
